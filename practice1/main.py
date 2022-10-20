b10ba8d8575902c42c8bbb85888f46f4b3973eab
MiKL <miklgaillard@gmail.com>
2022-10-20 16:46:38 +0200

    Practice 1

---
+++ practice1/main.py
@@ -0,0 +1,10 @@
+# 1. Imprimez votre âge dans la console.
+print("I've 33 yo.")
+# 2. Calculez et imprimez le nombre de jours, de semaines et de mois dans 27 ans. Ne vous inquiétez pas des années bissextiles !
+print(365 * 27)
+print(52 * 27)
+print(12 * 27)
+# 3. Calculez et imprimez l’aire d’un cercle dont le rayon est de 5 unités. Vous pouvez être aussi précis que vous le souhaitez avec la valeur de pi.
+print(3.141 * 5 * 5)
+print(3.141 * 5 ** 2)
+print(3.141 * pow(5,2) )
