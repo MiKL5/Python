de0cbfd4cafeb2fcd73f7dcd5473cf6cc6010b12
MiKL <miklgaillard@gmail.com>
2023-06-24 23:10:47 +0200

    Unpacking boucle For

---
+++ exercises/practice9/9.2_unpackingBoucleFor/main.py
@@ -0,0 +1,22 @@
+# Unpacking dans les boucles For
+
+movies = [
+    (
+        "Eternal Sunshine of the Spotless Mind",
+        "Michel Gondry",
+        2004
+    ),
+    (
+        "Memento",
+        "Christopher Nolan",
+        2000
+    ),
+    (
+        "Requiem for a Dream",
+        "Darren Aronofsky",
+        2000
+    )
+]
+
+for title, director, year in movies:
+    print(f"{title} ({year}), réalisé par {director}")
