d871d63311cc5df7145639c80083f34f041fd1b5
MiKL <miklgaillard@gmail.com>
2023-06-24 21:18:06 +0200

    Unpacking

---
+++ exercises/practice9/9.1_unpacking/main.py
@@ -0,0 +1,12 @@
+# Unpacking = déballage ou décomposition
+
+# Affecter les valeurs d'un tuples à trois variables
+
+movie = ("12 Angry Men", "Sidney Lumet", 1957)
+# title = movie[0]
+# director = movie[1]
+# year = movie[2]
+# plus simple et rapide
+title, director, year = movie
+
+# Le nombre de nom doit correspondre au nombre d'élément dans l'itérable, sinon, il y aura une erreur
