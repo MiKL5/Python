6b5207239a69b3881456684b2c13a10a7bf2a987
MiKL <miklgaillard@gmail.com>
2023-11-27 01:34:30 +0100

    🙈

--- drawGraphs/chart.py
+++
@@ -1,9 +0,0 @@
-from matplotlib import pyplot
-
-
-
-def createChart(x, y):
-    pyplot.scatter(x, y, alpha=0.5)
-    pyplot.savefig("graph.png")
-
-
--- drawGraphs/dataStorage.py
+++
@@ -1,7 +0,0 @@
-def readColumn(number):
-    columnData = []
-0    with open("iris.csv", "r") as iris:
-        for line in iris.readlines()[1:]:
-            data = line.strip().split(",")
-            columnData.append(data[number])
-    return columnData
