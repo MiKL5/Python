3a5c86414b3c1ed247038a71e921a305b3a9c566
MiKL <miklgaillard@gmail.com>
2023-12-06 12:23:26 +0100

    Generators and generating expressions

---
+++ exercises/practice23/23.1/main.py
@@ -0,0 +1,15 @@
+# Ecrire un générateur qui génére des nombres premiers dans un intervale spécifique
+
+def gen_primes(limit):
+    for dividend in range(2, limit + 1):
+        for divisor in range(2, dividend):
+            if dividend % divisor == 0:
+                break
+        else:
+            yield dividend
+
+
+primes = gen_primes(100) # Limite à 100
+
+for num in primes:
+    print(num)
